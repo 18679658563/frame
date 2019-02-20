60d391210d4d809bac6b0795b223466efe65b9fa
silence <m18679658563@163.com>
2019-02-20 17:29:10 +0800

    update

---
+++ src/main/java/com/e8/frame/service/impl/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.e8.frame.service.impl;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: silence
+ * Date: 2019-02-20
+ * Time: 下午5:27
+ */
+public class UserServiceImpl {
+}
