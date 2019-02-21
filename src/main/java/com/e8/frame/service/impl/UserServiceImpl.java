c1ceccd6817a9fa4c49fa7184a2922f98b98eb42
silence <m18679658563@163.com>
2019-02-21 15:49:02 +0800

    update

--- src/main/java/com/e8/frame/service/impl/UserServiceImpl.java
+++ src/main/java/com/e8/frame/service/impl/UserServiceImpl.java
@@ -1,7 +1,11 @@
 package com.e8.frame.service.impl;
 
 import com.e8.frame.mapper.UserMapper;
+import com.e8.frame.model.User;
+import com.e8.frame.model.dto.UserDto;
 import com.e8.frame.service.IUserService;
+import com.e8.frame.tools.BeanUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,4 +23,13 @@ public class UserServiceImpl implements IUserService {
     private UserMapper userMapper;
 
 
+    @Override
+    public UserDto findByUsername(String name) {
+        User user = userMapper.selectByUsername(name);
+        UserDto dto = null;
+        if(user != null){
+            dto = BeanUtil.createBeanByTarget(user,UserDto.class);
+        }
+        return dto;
+    }
 }
