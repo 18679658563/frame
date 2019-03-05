245b8b3d45182b987ddaf1e37758e99b3b6b484b
silence <m18679658563@163.com>
2019-03-05 10:18:24 +0800

    日志

--- src/main/java/com/e8/frame/service/impl/RedisServiceImpl.java
+++ src/main/java/com/e8/frame/service/impl/RedisServiceImpl.java
@@ -27,28 +27,43 @@ public class RedisServiceImpl implements IRedisService {
     private JedisPool pool;
 
     @Override
-    public PageUtil findByKey(String key, PageUtil page) {
+    public Object findByKey(String key, PageUtil page) {
         Jedis jedis = null;
         try{
             jedis = pool.getResource();
             List<RedisDto> redisDtos = new ArrayList<>();
-            if(!key.equals("*")){
+            if(!("*").equals(key)){
                 key = "*" + key + "*";
             }
-            if(StringUtils.isEmpty(key)){
-                key = "*";
-            }
             Set<String> set = jedis.keys(key);
-            for(String k : set){
+            List<String> redis = new ArrayList<>(set);
+            int total = 0;
+            if(page.getNumber() == 0){
+                if(redis.size()>=page.getSize()){
+                    total = page.getSize();
+                }else{
+                    total = redis.size();
+                }
+            }else {
+                if(redis.size() <= page.getNumber()){
+                    total = 0;
+                } else if(redis.size()/page.getSize()>0){
+                    total = page.getSize();
+                } else {//redis.size()%page.getNumber()>0
+                    total = redis.size()%page.getSize();
+                }
+            }
+            total = page.getNumber()+total>redis.size()?redis.size():page.getNumber()+total;
+            for(int x = page.getNumber();x < total ; x++){
                 RedisDto redisDto = new RedisDto();
-                redisDto.setKey(k);
-                redisDto.setValue(jedis.get(k));
+                redisDto.setKey(redis.get(x));
+                redisDto.setValue(jedis.get(redis.get(x)));
                 redisDtos.add(redisDto);
             }
-            List<RedisDto> redis =  PageUtil.toPage(page.getNumber(),page.getSize(),redisDtos);
-            page.setList(redis);
-            page.setCount(redisDtos.size());
-            return page;
+           // List<RedisDto> redis =  PageUtil.toPage(page.getNumber(),page.getSize(),redisDtos);
+            page.setList(redisDtos);
+            page.setCount(redis.size());
+            return PageUtil.toResult(page);
         } finally {
             if(jedis != null){
                 jedis.close();
